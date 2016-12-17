24c71afd5874a60a82bbe7282d50db8505a30514
jallen89 <jallen89@vols.utk.edu>
2016-12-16 19:21:39 -0500

    Added 25

--- getAllPlats.sh
+++ getAllPlats.sh
@@ -5,12 +5,9 @@ platform_template="platforms;android-"
 #Check if ANDROID_HOME set.
 : "${ANDROID_HOME:?ANDROID_HOME is not set.}"
 
-sdkmanager="$ANDROID_HOME/tools/bin/sdkmanager"
-
-
 # Get all platforms.
 for i in {1..25}; do 
-	cmd="$sdkmanager \"$platform_template$i\""
+	cmd="$ANDROID_HOME/tools/bin/sdkmanager \"$platform_template$i\""
 	echo $cmd
 	eval $cmd
 done
